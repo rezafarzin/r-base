40ec781fb2c3f907a64e7dea91d9cc4bb17e80a8
Reza Farzin <reza.farzin@gmail.com>
2020-03-03 16:12:00 -0700

    Move some of the code from .sh to Dockerfile. Added python to the image

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,28 @@ ENV TZ=UTC
 ENV DEBIAN_FRONTEND=noninteractive
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 RUN apt-get update
-RUN apt-get install -y --no-install-recommends apt-utils
+RUN apt-get install -y --no-install-recommends apt-utils software-properties-common
+
+# "Adding new R versin 3.6"
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+RUN add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+
+#Install pre-requisite for R packages
+RUN apt-get -y install \
+    curl \
+    libxml2-dev \
+    libssl-dev \
+    libcurl4-openssl-dev \
+    openjdk-8-jdk-headless \
+    curl \
+    tar \
+    vim \
+    tree \
+    apt-transport-https \
+    gnupg \
+    lsb-release \
+    wget \
+    python
 
 COPY ./setup.sh /setup.sh
 COPY ./Packages.R /Packages.R
--- setup.sh
+++ setup.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-echo "Adding new R versin 3.6"
-apt-get install -y software-properties-common
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
-
-echo "Installing all required packages..."
-#Install pre-requisite for R packages
-apt-get update
-apt-get -y upgrade
-apt-get -y install curl
-apt-get -y install libxml2-dev
-apt-get -y install libcurl4-openssl-dev
-apt-get -y install libssl-dev
-apt-get -y install openjdk-8-jdk-headless
-apt-get -y install curl
-apt-get -y install tar
-apt-get -y install vim
-apt-get -y install tree
-apt-get -y install apt-transport-https
-apt-get -y install gnupg
-apt-get -y install lsb-release
-apt-get -y install wget
-
-
 # from https://arrow.apache.org/install/
 apt update
 apt install -y -V lsb-release wget
@@ -44,7 +20,6 @@ apt install -y -V libgandiva-dev # For Gandiva C++
 apt install -y -V libgandiva-glib-dev # For Gandiva GLib (C)
 apt install -y -V libparquet-dev # For Apache Parquet C++
 apt install -y -V libparquet-glib-dev # For Apache Parquet GLib (C)
-
 
 #Install R and dependecies
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
